75fc80e28a135e1816468471bbf0c25492fb40c6
Adam Richards <adamricha@gmail.com>
2013-11-20 16:38:50 +0100

    reworking gograph creation

--- src/GeneOntology.py
+++ src/GeneOntology.py
@@ -14,40 +14,69 @@ The data is stored in the htsint database
 class GeneOntology(object):
     "A class to interact with Gene Ontology data"
     
-    def __init__(self,taxID,verbose=False):
+    def __init__(self,taxID=None,geneList=None,verbose=False):
         """
         Constructor
         
         taxID - is the NCBI taxid (int)
                 i.e. for xenopus
                 go = GeneOntology(8364)
+
+                if no taxID is supplied then the class is in 'mixed' taxa mode
+                and a geneList must be supplied.
+        geneList - a list of ncbi gene ids
+
         """
 
-        ## variables
-        self.taxID = taxID
+        ## error checking
+        if taxID == None and geneList == None:
+            raise Exception("If taxid is not supplied a 'geneList' is required")
+
+        ## start a database session
         self.session,self.engine = db_connect(verbose=verbose)
 
-        ## check if taxon is in database
-        self.taxQuery = self.session.query(Taxon).filter_by(ncbi_id=self.taxID).first()
-        if self.taxQuery == None:
-            raise Exception("Taxon:%s not found in database"%self.taxID)
+        if geneList != None:
+            self.geneList = geneList
+            self.taxID = None
+        else:
+            self.taxID = taxID
+            self.check_taxon(taxID)
+            self.taxQuery = self.session.query(Taxon).filter_by(ncbi_id=self.taxID).first()
+            self.geneQuery = self.session.query(Gene).filter_by(taxa_id=self.taxQuery.id)
+            self.geneList = [g.ncbi_id for g in self.geneQuery.all()]
+            self.geneQuery = self.session.query(Gene).filter_by(taxa_id=self.taxQuery.id)
+            self.annotQuery = self.session.query(GoAnnotation).filter_by(taxa_id=self.taxQuery.id)
+        
+            print self.geneList[:30]
+    def check_taxon(self,taxID):
+        """
+        check if taxon is in database
+        """
 
-        ## other database queries
-        self.geneQuery = self.session.query(Gene).filter_by(taxa_id=self.taxQuery.id)
-        self.annotQuery = self.session.query(GoAnnotation).filter_by(taxa_id=self.taxQuery.id)
+        taxQuery = self.session.query(Taxon).filter_by(ncbi_id=taxID).first()
+        if taxQuery == None:
+            raise Exception("Taxon:%s not found in database"%taxID)
         
     def print_summary(self):
         """
         Print a summary of all Gene Ontology related information in database
         """
-        print "\nGene Ontology Summary"
-        print "----------------------------------"
-        print "TaxID:       %s"%self.taxQuery.ncbi_id
-        print "Species:     %s"%self.taxQuery.name
-        print "Common Name: %s"%self.taxQuery.common_name_1
-        print "Num. Genes:  %s"%self.geneQuery.count()
-        print "Num. GO Annotations:  %s"%self.annotQuery.count()
-        print "\n"
+
+        if self.taxID != None:
+            print "\nSummary"
+            print "----------------------------------"
+            print "TaxID:       %s"%self.taxQuery.ncbi_id
+            print "Species:     %s"%self.taxQuery.name
+            print "Common Name: %s"%self.taxQuery.common_name_1
+            print "Num. Genes:  %s"%self.geneQuery.count()
+            print "Num. GO Annotations:  %s"%self.annotQuery.count()
+            print "\n"
+        else:
+            print "\nSummary"
+            print "----------------------------------"
+            print "TaxID:       mixed taxa"
+            print "Num. Genes:  %s"%len(self.geneList)
+            print "\n"
 
     def create_gograph(self,aspect='biological_process',geneList=None):
         """
@@ -64,36 +93,80 @@ class GeneOntology(object):
         compEvidCodes = ["ISS","ISO","ISA","ISM","IGC","RCA"]
         statEvidCodes = ["TAS","NAS","IC"]
         nonCuratedEvidCodes = ["IEA"]
-        _goDict = read_ontology_file()    
-        goDict = _goDict['biological_process']
+        #_goDict = read_ontology_file()    
+        #goDict = _goDict['biological_process']
 
         ## error checking
         if aspect not in ['biological_process','molecular_function','cellular_component']:
             raise Exception("Invalid aspect specified%s"%aspect)
 
-        ## get the gene list
-        if geneList == None:
-            geneList = [g.symbol for g in self.geneQuery.all()]
-
         ## get the terms associated with each gene
-        go2gene = {}
-        for item in self.annotQuery.all():
-            ## include only certain evidence codes
-            if item.evidence_code  in nonCuratedEvidCodes or item.evidence_code in compEvidCodes:
-                continue
-
-            ## include only the correct aspect
-            termQuery = self.session.query(GoTerm).filter_by(id=item.go_term_id).first()
-            if termQuery.aspect != shortAspect[aspect]:
-                continue
+        print "making gene2go"
+        gene2go = {}
+        totalAnnotations = 0
+        for gene in self.geneList:
+            geneQuery = self.session.query(Gene).filter_by(ncbi_id=gene).first()
+            if geneQuery == None:
+                raise Exception("A specified gene is not in the database%"%gene)
+            annotQuery = self.session.query(GoAnnotation).filter_by(gene_id=geneQuery.id)
             
-            ## update the dictionary
-            symbol = self.geneQuery.filter_by(id=item.gene_id).first().symbol
-            if go2gene.has_key(termQuery.go_id) == False:
-                go2gene[termQuery.go_id] = set([])
-            go2gene[termQuery.go_id].update([symbol])
+            if annotQuery.count() > 0:
+                annotations = set([])
+                for a in annotQuery.all():
+                    termQuery = self.session.query(GoTerm).filter_by(id=a.go_term_id).first()
+                    
+                    ## filter by evidence code
+                    if a.evidence_code in nonCuratedEvidCodes or a.evidence_code in compEvidCodes:
+                        continue
+                    
+                    ## filter by aspect
+                    if termQuery.aspect != shortAspect[aspect]:
+                        continue
+
+                    annotations.update([termQuery.go_id])
+                if len(annotations) > 0:
+                    gene2go[gene] = list(annotations)
+                    totalAnnotations += len(annotations)
+
+        ## get the genes associated with each term
+        print "making go2gene"
+        go2gene = {}
+        for gene,terms in gene2go.iteritems():
+            for term in terms:
+                if go2gene.has_key(term) == False:
+                    go2gene[term] = set([])
+                go2gene[term].update([gene])
+
+        for term,genes in go2gene.iteritems():
+            go2gene[term] = list(genes)
+
+
+        print 'go2gene', go2gene.keys()
+        print 'gene2go', gene2go.keys()
+
+       #print go2gene
+        #print gene2go
+        #print totalAnnotations
+        #
+        #sys.exit()
+
+        ## include only certain evidence codes
+        #if item.evidence_code  in nonCuratedEvidCodes or item.evidence_code in compEvidCodes:
+        #    continue
+        
+        ## include only the correct aspect
+        #termQuery = self.session.query(GoTerm).filter_by(id=item.go_term_id).first()
+        #if termQuery.aspect != shortAspect[aspect]:
+        #    continue
+            
+        ## update the dictionary
+        #symbol = self.geneQuery.filter_by(id=item.gene_id).first().symbol
+        #if go2gene.has_key(termQuery.go_id) == False:
+        #    go2gene[termQuery.go_id] = set([])
+        #go2gene[termQuery.go_id].update([symbol])
         
         ## calculate the information content for each term -ln(p(term))
+        """
         total = 0
         for term,genes in go2gene.iteritems():
             total += len(list(genes))
@@ -125,7 +198,7 @@ class GeneOntology(object):
         filePath = os.path.join(__basedir__,'graphs','gograph_%s.pickle'%(self.taxQuery.ncbi_id))
         nx.write_gpickle(G, filePath)
         print 'saving pickle graph'                      
-
+        """
 
     def get_weight_by_ic(term):
         """
@@ -140,7 +213,19 @@ if __name__ == "__main__":
     Mus musculus            - 10090
     Drosophila melanogaster - 7227
     """
-    
-    go = GeneOntology(7227)
+
+    ## testing with a species
+    go = GeneOntology(taxID=7227)
+    go.print_summary()
+    #go.create_gograph()
+
+    ## testing with a gene list
+    geneList = ['30970','30971','30972','30973','30975','30976','30977','30978','30979','30980',
+                '30981','30982','30983','30984','30985','30986','30988','30990','30991','30994',
+                '30995','30996','30998','31000','31001','31002','31003','31004','31005','31006']
+
+    go = GeneOntology(geneList=geneList)
     go.print_summary()
     go.create_gograph()
+
+    #go.create_gograph()
