068a7e3e13a4f7b7276df7fc63f441a163026ed4
Adam Richards <adamricha@gmail.com>
2014-04-01 10:50:57 +0200

    modified setup and ignore for src name change

--- setup.py
+++ setup.py
@@ -1,38 +1,44 @@
-'''
-Created Oct 24th, 2013
-'''
+#!/usr/bin/env python
 
-import os,sys
-from distutils.core import setup
-from distutils.extension import Extension
-from numpy import get_include
-
-sys.path.append("src")
-from version import __version__
-
-__author__ = "AJ Richards"
-
-from numpy.distutils.misc_util import Configuration
+import os,sys,re
+from distutils.sysconfig import get_python_lib
+from shutil import copytree
 from numpy.distutils.core import setup
+
 try:
     from py2exe.build_exe import py2exe
 except:
     pass
 
-sys.path.append("lpEdit")
+sys.path.append("htsint")
 from version import __version__
 
-DESCRIPTION = "Cross-platform editor to facilitate literate programming"
-LONG_DESCRIPTION = """                                                                                                                                                                                            
+__author__ = "AJ Richards"
 
+DESCRIPTION = "API and database for high-throughput sequencing analyses."
+LONG_DESCRIPTION = """
 A package meant to construct Bayesian models that aid in the discovering of
 biomarkers via the integration high-throughput data sources with additional 
 covariates.
 
 """
 
+def get_files(dirPath):
+    notIncluded = ["\.pyc"]
+    allFiles = []
+    for fileName in os.listdir(dirPath):
+        include = True
+        for pat in notIncluded:
+            if re.search(pat,fileName):
+                include = False
+        filePath = os.path.join(dirPath,fileName)
+        if include == True and os.path.isfile(filePath):
+            allFiles.append(os.path.realpath(filePath))
+            
+    return allFiles
+
 REQUIRES = ['numpy', 'matplotlib','pymc']
-DISTNAME = 'lpEdit'
+DISTNAME = 'htsint'
 LICENSE = 'BSD 3-Clause'
 AUTHOR = "Adam J Richards"
 AUTHOR_EMAIL = "adamricha@gmail.com"
@@ -46,33 +52,30 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD 3-Clause'
 ]
 
+DATADIRS = ["database","blast","stats","tools"]
+FILES = {'': [os.path.join("htsint","*.py")],
+         'database': [os.path.join("htsint","database","*.py")],
+         'blast': [os.path.join("htsint","blast","*.py *.pl")],
+         'stats': [os.path.join("htsint","stats","*.py")],
+         'tools':[os.path.join("htsint","tools","*.py")]}
+
 ISRELEASED = True
 VERSION = __version__
 FULLVERSION = VERSION
 if not ISRELEASED:
     FULLVERSION += '.beta'
 
-def configuration(parent_package='', top_path=None):
-    config = Configuration(None, parent_package, top_path,
-                           version=FULLVERSION)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-
-    config.add_subpackage('src')    
-    config.add_data_dir(os.path.join('src','examples'))
-
-    return config
 
 if __name__ == '__main__':
     setup(name=DISTNAME,
           author=AUTHOR,
           author_email=AUTHOR_EMAIL,
           description=DESCRIPTION,
+          version=FULLVERSION,
           license=LICENSE,
           url=URL,
+          package_dir={'': 'htsint'},
           long_description=LONG_DESCRIPTION,
           classifiers=CLASSIFIERS,
-          platforms='any',
-          configuration=configuration)
+          package_data= FILES,
+          platforms='any')
