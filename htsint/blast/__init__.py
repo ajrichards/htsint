7c30e6ee68c06eed4d914070239cccb62a1d99ca
Adam Richards <adamricha@gmail.com>
2014-12-05 17:18:11 +0100

    added BlastMapper import

--- htsint/blast/__init__.py
+++ htsint/blast/__init__.py
@@ -5,6 +5,7 @@ blast related classes and functions
 
 from BlastTools import create_blast_map,get_blast_map
 from Blast import Blast
+from BlastMapper import BlastMapper
 from ParallelBlast import ParallelBlast
 from ParseBlast import ParseBlast
 from ParseParallelBlast import ParseParallelBlast
